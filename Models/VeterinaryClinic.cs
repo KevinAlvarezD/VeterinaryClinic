0550a60e680bcb1e0f278aee38b5d582f60a09f4
KevinAlvarezA <kevinadozanam23@gmail.com>
2024-08-05 06:49:23 -0500

    feat: Se crea la clase Cat

--- Models/VeterinaryClinic.cs
+++ Models/VeterinaryClinic.cs
@@ -9,7 +9,7 @@ public class VeterinaryClinic
 {
     public string Name { get; set; }
     public string Address { get; set; }
- /*    public List<Dog> Dogs { get; set; } = new List<Dog>();
-    public List<Cat> Cats { get; set; } = new List<Cat>(); */
+    public List<Dog> Dogs { get; set; } = new List<Dog>();
+    /* public List<Cat> Cats { get; set; } = new List<Cat>(); */
 
 }
