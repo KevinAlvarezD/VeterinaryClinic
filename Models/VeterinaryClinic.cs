e7ff96aef6d2e82de016928ee788f48f95a0ea17
KevinAlvarezA <kevinadozanam23@gmail.com>
2024-08-05 06:41:01 -0500

    feat: se agregan atributos a la clase veterinary

--- Models/VeterinaryClinic.cs
+++ Models/VeterinaryClinic.cs
@@ -7,5 +7,9 @@ namespace VeterinaryCenter.Models;
 
 public class VeterinaryClinic
 {
+    public string Name { get; set; }
+    public string Address { get; set; }
+ /*    public List<Dog> Dogs { get; set; } = new List<Dog>();
+    public List<Cat> Cats { get; set; } = new List<Cat>(); */
 
 }
